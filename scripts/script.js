1ce86dd6147c1afc9caf13d45f1ad18e8db9f4c6
Masih Saldanha Xavier <masih.saldanha@gmail.com>
2022-02-24 13:35:25 -0300

    Menu principal e artigos feitos, falta conteúdo

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-console.log("teste");
+const MENU = document.querySelector("main");
+const LISTA_ARTIGOS = [];
+for (i = 0; i < 7; i++) {
+    LISTA_ARTIGOS[i] = document.querySelector(`.artigo-${i}`);
+}
+
+function acessarArtigo(numeroArtigo) {
+    LISTA_ARTIGOS[numeroArtigo].classList.remove("escondido")
+    MENU.classList.add("escondido")
+}
+
+function voltarMenuPrincipal(numeroArtigo) {
+    LISTA_ARTIGOS[numeroArtigo].classList.add("escondido")
+    MENU.classList.remove("escondido")
+}
